b03444d34f2bfdba835de18eaf1b20cc68890043
Coilice3 <tranvu8398@gmail.com>
2022-10-13 20:53:27 +0700

    profile

---
+++ profile/info.js
@@ -0,0 +1,6 @@
+function result(){
+    let y = document.getElementById('year').value
+    let m = document.getElementById('month').value
+    let d = document.getElementById('day').value
+    document.write(d + '-' + m + '-' + y)
+}
